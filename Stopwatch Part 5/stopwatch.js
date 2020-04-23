2a46914f9192eb9a6e998172ba9b4542b87af302
Omri Nathanson <omri.nathanson@gmail.com>
2020-04-23 12:35:48 +0300

    refactor(concise code): change code wherever possible to be more concise (1 element for the elapsed display instead of multiple, spares in element's classes and css etc.)

--- Stopwatch Part 5/stopwatch.js
+++ Stopwatch Part 5/stopwatch.js
@@ -1,42 +1,36 @@
-const MINUTES_CLASS_NAME = "minutes";
-const SECONDS_CLASS_NAME = "seconds";
-const MILLISECONDS_CLASS_NAME = "milliseconds";
-const START_BUTTON_CLASS_NAME = "start-button";
-const STOP_BUTTON_CLASS_NAME = "stop-button";
-let displayElapsedInterval;
+let elapsedDisplay = document.getElementsByClassName("elapsed-display")[0];
+let startButton = document.getElementsByClassName("start-button")[0];
+let stopButton = document.getElementsByClassName("stop-button")[0];
+let updateElapsedInterval;
 
 bindControlsEvents();
 
 function bindControlsEvents() {
-    document.getElementsByClassName("start-button")[0].onclick = onStart;
-    document.getElementsByClassName("stop-button")[0].onclick = onStop;
+    startButton.onclick = onStart;
+    stopButton.onclick = onStop;
 }
 
 function onStart() {
-    resetTime()
-    this.disabled = true;
-    document.getElementsByClassName("stop-button")[0].disabled = false;
-    displayElapsedInterval = setInterval(displayElapsed, 1, Date.now())
+    elapsedDisplay.innerHTML = "00:00:000";
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    updateElapsedInterval = setInterval(updateElapsed, 1, Date.now())
 }
 
-function displayElapsed(startingTime) {
+function updateElapsed(startingTime) {
     let elapsedMilliseconds = Date.now() - startingTime;
-    document.getElementsByClassName(MILLISECONDS_CLASS_NAME)[0].innerHTML =
-        (elapsedMilliseconds % 1000).toString().padStart(3, '0');
-    document.getElementsByClassName(SECONDS_CLASS_NAME)[0].innerHTML =
-        (Math.floor(elapsedMilliseconds % 60000 / 1000)).toString().padStart(2, '0');
-    document.getElementsByClassName(MINUTES_CLASS_NAME)[0].innerHTML = 
-        (Math.floor(elapsedMilliseconds / 60000)).toString().padStart(2, '0');
+    let minutes = padWithZeros(Math.floor(elapsedMilliseconds / 60000), 2);
+    let seconds = padWithZeros(Math.floor(elapsedMilliseconds % 60000 / 1000), 2);
+    let milliseconds = padWithZeros(elapsedMilliseconds % 1000, 3);
+    elapsedDisplay.innerHTML = minutes + ":" + seconds + ":" + milliseconds;
+}
+
+function padWithZeros(number, minimumLength) {
+    return number.toString().padStart(minimumLength, '0');
 }
 
 function onStop() {
-    clearInterval(displayElapsedInterval);
-    this.disabled = true;
-    document.getElementsByClassName("start-button")[0].disabled = false;
-}
-
-function resetTime() {
-    document.getElementsByClassName(MINUTES_CLASS_NAME)[0].innerHTML = "00";
-    document.getElementsByClassName(SECONDS_CLASS_NAME)[0].innerHTML = "00";
-    document.getElementsByClassName(MILLISECONDS_CLASS_NAME)[0].innerHTML = "000";
+    clearInterval(updateElapsedInterval);
+    stopButton.disabled = true;
+    startButton.disabled = false;
 }
