59bfee2002b8aa58a0032273c9b82a35c5a84fc0
Omri Nathanson <omri.nathanson@gmail.com>
2020-05-05 16:10:20 +0300

    refactor(import for json, es6 features, eslint cofig): change the importing of json file, shorten code (with more es6 features), config eslint (so now eslint auto-runs before 'npm start').

--- Dive into JS Part 6/heroes.js
+++ Dive into JS Part 6/heroes.js
@@ -1,63 +1,40 @@
-function getHeroes() {
-  const heroesData = jsonLoader.sync("ow.json");
-  const names = heroesData.names;
-  const roles = heroesData.roles;
-  const hps = heroesData.hp;
+import heroesData from './ow.json';
+
+const getHeroes = () => {
+  const { names, roles, hp } = heroesData;
 
   return names.reduce((heroes, name, index) => {
-    heroes.push({
-      name: name,
-      role: roles[index],
-      hp: hps[index],
-    });
+    heroes.push({ name, role: roles[index], hp: hp[index] });
 
     return heroes;
   }, []);
-}
+};
 
-function groupBy(heroes, category = roleCategory) {
-  return heroes.reduce((groupedHeroes, hero) => {
-    if (!groupedHeroes.hasOwnProperty(hero[category])) {
-      groupedHeroes[hero[category]] = [];
-    }
-
-    groupedHeroes[hero[category]].push(hero);
+const groupBy = (heroes, category = 'role') =>
+  heroes.reduce((groupedHeroes, hero) => {
+    (groupedHeroes[hero[category]] = groupedHeroes[hero[category]] || []).push(hero);
 
     return groupedHeroes;
   }, {});
-}
 
-function getByRoles(heroes, ...roles) {
-  return heroes.filter(hero => roles.includes(hero[roleCategory]));
-}
+const getByRoles = (heroes, ...roles) =>
+  heroes.filter(hero => roles.includes(hero.role));
 
-const makeHeroesNice = (function () {
-  function sayHello() {
-    console.log(`Hi! My name is ${this.name}, nice to meet you!`);
-  }
+const makeHeroesNice = heroes =>
+  heroes.map(hero =>
+    ({ ...hero, sayHello: () => { console.log(`Hi! My name is ${hero.name}, nice to meet you!`); } })
+  );
 
-  return function (heroes) {
-    heroes.forEach((hero) => {
-      hero.sayHello = sayHello;
-    });
-
-    return heroes;
-  };
-})();
-
-import jsonLoader from 'load-json-file';
-const roleCategory = "role";
 const heroes = getHeroes();
-
-console.log("Here are all the heroes:");
-console.table(heroes);
-console.log("\nThe heroes grouped by default (role):");
-console.log(groupBy(heroes));
-console.log("\nThe heroes grouped by hp:");
-console.log(groupBy(heroes, "hp"));
-console.log("\nThe heroes with roles of offense or tank:");
-console.table(getByRoles(heroes, "Offense", "Tank"));
-
-console.log("\nThe second hero says hello:");
 const niceHeroes = makeHeroesNice(heroes);
+
+console.log('Here are all the heroes:');
+console.table(heroes);
+console.log('\nThe heroes grouped by default (role):');
+console.log(groupBy(heroes));
+console.log('\nThe heroes grouped by hp:');
+console.log(groupBy(heroes, 'hp'));
+console.log('\nThe heroes with roles of offense or tank:');
+console.table(getByRoles(heroes, 'Offense', 'Tank'));
+console.log('\nThe second hero says hello:');
 niceHeroes[1].sayHello();
