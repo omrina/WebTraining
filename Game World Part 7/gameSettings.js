4e351dedd353fc814a0f8f87944764a28852d2f3
Omri Nathanson <omri.nathanson@gmail.com>
2020-05-11 11:05:40 +0300

    feat(right click feature): add right click (on tiles) that switches their state between unmarked, flagged, question marked (w/ the latter being able to be disabled later on)

--- Game World Part 7/gameSettings.js
+++ Game World Part 7/gameSettings.js
@@ -1,4 +1,6 @@
 const easyMode = { rowsCount: 9, columnsCount: 9, minesCount: 10 };
 const mediumMode = { rowsCount: 16, columnsCount: 16, minesCount: 40 };
 const hardMode = { rowsCount: 16, columnsCount: 30, minesCount: 99 };
-export let settings = {...easyMode, questionMarking: true };
+const questionMarkingClass = 'questioned-tile';
+const markingStateClasses = ['unmarked', 'flagged-tile', questionMarkingClass];
+export let settings = {...easyMode, markingStateClasses};
--- Game World Part 7/minesweeper.js
+++ Game World Part 7/minesweeper.js
@@ -1,17 +1,23 @@
 import { settings } from './gameSettings.js' ;
 {
   const initializeBoard = () => {
-    const [isRevealed, isMarked, isMine] = [false, false, false];
+    const [isRevealed, isMine, markingStates] = [false, false, [...settings.markingStateClasses]];
 
     return Array(settings.rowsCount).fill().map(() =>
-     Array(settings.columnsCount).fill().map(() => Object.assign({}, { isRevealed, isMarked, isMine })));
+              Array(settings.columnsCount).fill().map(() =>
+                 Object.assign({}, { isRevealed,
+                                     isMine,
+                                     markingStates,
+                                     currentMarkState () { return markingStates[0]; },
+                                     isMarked () { return currentMarkState() !== ''; }
+                                    })));
   };
 
   const generateMinesLocations = minesAmount => {
     const locations = [];
 
     while (locations.length < minesAmount) {
-        let newLocation = [Math.floor(Math.random() * settings.rowsCount), Math.floor(Math.random() * settings.columnsCount)]
+        const newLocation = [Math.floor(Math.random() * settings.rowsCount), Math.floor(Math.random() * settings.columnsCount)]
         
         if (!locations.some(location => location[0] === newLocation[0] && location[1] === newLocation[1])){
             locations.push(newLocation);
@@ -26,4 +32,29 @@ import { settings } from './gameSettings.js' ;
           board[location[0]][location[1]].isMine = true;
       });
   }
+
+  const getLocation = tile => {
+    const parentRow = tile.parentNode;
+
+    return [[...parentRow.parentNode.children].indexOf(parentRow),
+            [...parentRow.children].indexOf(tile)];
+  }
+
+  const onRightClick = (tile, board) => {
+    const tileLocation = getLocation(tile);
+    const {currentMarkState, markingStates} = { ...board[tileLocation[0]][tileLocation[1]] };
+
+    tile.classList.remove(currentMarkState())
+    markingStates.push(markingStates.shift())
+    tile.classList.add(currentMarkState())
+  }
+
+  let board = initializeBoard();
+  // Add ('draw') the board (all tiles) in the html?
+  // Bind click events!!!
+  [...document.getElementsByClassName('tile')].forEach(tile => tile.oncontextmenu = event => {
+    event.preventDefault();
+    onRightClick(event.target, board);
+  });
+  // Bind click events!!!
 }
