bf376697a9641c2cdb09d7a5fd7658d14e0993c7
Omri Nathanson <omri.nathanson@gmail.com>
2020-05-11 14:48:14 +0300

    feat(one left-click): add handling of one left-click on a tile, which won't open if it's marked. Right now opening a mine will just alert a message, and there is not a automatic revealing if opened a 0-mine neighbors tile (yet).

--- Game World Part 7/minesweeper.js
+++ Game World Part 7/minesweeper.js
@@ -7,9 +7,9 @@ import { settings } from './gameSettings.js' ;
               Array(settings.columnsCount).fill().map(() =>
                  Object.assign({}, { isRevealed,
                                      isMine,
-                                     markingStates,
-                                     currentMarkState () { return markingStates[0]; },
-                                     isMarked () { return currentMarkState() !== ''; }
+                                     markingStates: [...markingStates],
+                                     currentMarkState () { return this.markingStates[0]; },
+                                     isMarked () { return this.currentMarkState() !== 'unmarked'; }
                                     })));
   };
 
@@ -41,20 +41,71 @@ import { settings } from './gameSettings.js' ;
   }
 
   const onRightClick = (tile, board) => {
-    const tileLocation = getLocation(tile);
-    const {currentMarkState, markingStates} = { ...board[tileLocation[0]][tileLocation[1]] };
+    const [row, col] = getLocation(tile);
+    const tileData = board[row][col];
 
-    tile.classList.remove(currentMarkState())
-    markingStates.push(markingStates.shift())
-    tile.classList.add(currentMarkState())
+    tile.classList.remove(tileData.currentMarkState())
+    tileData.markingStates.push(tileData.markingStates.shift())
+    tile.classList.add(tileData.currentMarkState())
+  }
+
+  const revealIfNotMarked = (tile, board, revealFunction) => {
+    const [row, col] = getLocation(tile);
+
+    if (!board[row][col].isMarked()) {
+      revealFunction();
+    }
+  }
+
+  const revealTile = (tile, board) => {
+    const numbersClasses = ['zero', 'number-one', 'number-two', 'number-three', 'number-four', 'number-five', 'number-six', 'number-seven', 'number-eight'];
+    const neighbors = getNeighborsData([...getLocation(tile)], board);
+    const minesCount = neighbors.filter(x => x.isMine).length;
+    tile.classList.add(numbersClasses[minesCount])
+    tile.classList.remove('unrevealed-tile')
+  }
+
+  const revealMinedTile = (tile, board) => {
+      // lose scenario!!!
+      alert('loser!')
+  }
+
+  
+
+  const getNeighborsData = ([row, col], board) => {
+    // const [row, col] = getLocation(tile);
+    
+    return board.slice(row === 0 ? 0 : row-1, row+2).reduce((neighbors, currentRow) => 
+              [...neighbors, ...currentRow.slice(col === 0 ? 0 : col-1, col+2)], [])
+              .filter(x => x !== board[row][col]);
   }
 
   let board = initializeBoard();
+  let a = generateMinesLocations(settings.minesCount);
+  assignMinesOnBoard(board, a);
+
+  console.log(a);
   // Add ('draw') the board (all tiles) in the html?
   // Bind click events!!!
+
+  // binding right click
   [...document.getElementsByClassName('tile')].forEach(tile => tile.oncontextmenu = event => {
     event.preventDefault();
     onRightClick(event.target, board);
+  });
+
+  // binding left click
+  [...document.getElementsByClassName('tile')].forEach(tile => tile.onclick = event => {
+    revealIfNotMarked(event.target, board, () => revealTile(event.target, board));
+  });
+
+  // bind revealMine to the mined locations
+  [...document.getElementsByClassName('tile')].filter(tile => {
+    const [row, col] = getLocation(tile);
+
+    return board[row][col].isMine;
+  }).forEach(minedTile => minedTile.onclick = event => {
+    revealIfNotMarked(event.target, board, () => revealMinedTile(event.target, board));
   });
   // Bind click events!!!
 }
